e048cd99dbca58ce68734b98a652358b4c6a14b2
tmrstata <tmrs@outlook.com>
2023-01-31 17:08:42 -0300

    git commit -m ''Revisão"

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,25 @@
-const cipher = {
-  // ...
-};
+const alfabeto = "ABCDEFGHIJKLMNOPQRSTUVXYZ" 
 
+const cipher = {
+  encode: function encode (offset, mensagem) {
+
+    if (offset == null) {
+      throw new TypeError("Escolha um número de deslocamento");
+    } else {
+      let resultadoCifra = "";
+      for (let i = 0; i < string.length; i++) {
+        const cifra = string.charCodeAt(i);
+        let codificar =
+          ((cifra - codigoAscii + offset) % tamanhoAlfabeto) + codigoAscii;
+        if (cifra <= 64 || cifra >= 91) {
+          codificar = cifra;
+        }
+        resultadoCifra += String.fromCharCode(codificar);
+      } 
+      return resultadoCifra;
+    }
+    
+  } 
+}
 export default cipher;
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+/*Cifra de Cesar
+
+A  B  C  D  E  F  G  H  I  J  K  L  M 
+1  2  3  4  5  6  7  8  9 10 11 12 13 
+
+N   O   P   Q   R  S  T  U  V  W   X  Y  Z
+13 14  15  16  17  18 19 20 21 22 23 24 25
+
+*/
